c05036ea6dc555165de3cdb4aa11e37a2cfe921f
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-11 12:50:37 +0330

    Add files via upload

---
+++ Battle 29 - Font/script.js
@@ -0,0 +1,24 @@
+const container = document.getElementById('container')
+let list = [
+    "01 Tanzania",
+    "02 Kuwait",
+    "03 Iceland",
+    "04 Bahrain",
+    "05 Brazil"
+]
+
+
+for (let i = 0; i < list.length; i++) {
+    container.innerHTML += `
+        <div class="project">
+            <div class="line">
+                <h2>${list[i].split(" ")[1]}</h2>
+                <a href="${list[i]}/index.html">
+                    <button type="button">Go to Site</button>
+                </a>
+            </div>
+            <img src="${list[i]}/screen.jpg" />
+        </div>
+    `
+}
+
