a00cd266bfb716e8c3339dbf818453d178a36648
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-28 13:24:59 +0330

    Update script.js

--- Daily Targets/script.js
+++ Daily Targets/script.js
@@ -1,14 +1,18 @@
+// const sepContainer = document.getElementById('sepContainer')
+// const octContainer = document.getElementById('octContainer')
+// const novContainer = document.getElementById('novContainer')
+// const decContainer = document.getElementById('decContainer')
 let day = 1
 
-function makeMonth( month, start, end ) {
-    let output = ``
+
+function makeMonth(month, start, end) {
     for (let i = start; i < end; i++) {
         day = day < 10? `0${day}`: day
         const date = `${month}${i < 10? `0${i}`: i}`
 
         if (date == 'Nov05') { continue }
         else {
-            output += `
+            container.innerHTML += `
                 <div class="project">
                     <div class="line">
                         <h2>${date}</h2>
@@ -22,22 +26,17 @@ function makeMonth( month, start, end ) {
             day++
         }
     }
-    if (month == 'Sep') {
-        document.getElementById('sepContainer').innerHTML = output
-    }
-    else if (month == 'Oct') {
-         document.getElementById('octContainer').innerHTML = output
-    }
-    else if (month == 'Nov') {
-         document.getElementById('novContainer').innerHTML = output
-    }
-    else if (month == 'Dec') {
-         document.getElementById('decContainer').innerHTML = output
-    }
+    // if (month == 'Sep') { sepContainer.innerHTML = output }
+    // else if (month == 'Oct') { octContainer.innerHTML = output }
+    // else if (month == 'Nov') { novContainer.innerHTML = output }
+    // else if (month == 'Dec') { decContainer.innerHTML = output }
 }
 
+if ( document.getElementById('sepContainer') ) {
+    const container = document.getElementById('sepContainer')
+    makeMonth('Sep', 8, 31)
+}
 
-makeMonth('Sep', 8, 31)
-makeMonth('Oct', 1, 32) 
-makeMonth('Nov', 1, 31)
-makeMonth('Dec', 1, 29)   // CHANGE //
+// makeMonth('Oct', 1, 32) 
+// makeMonth('Nov', 1, 31)
+// makeMonth('Dec', 1, 29)   // CHANGE //
