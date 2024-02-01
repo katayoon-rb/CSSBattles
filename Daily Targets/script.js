1c41b3e234bcb8acbe8e76e06e991c9147eba8d5
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-02-01 12:15:38 +0330

    Update script.js

--- Daily Targets/script.js
+++ Daily Targets/script.js
@@ -36,3 +36,7 @@ else if ( document.getElementById('dec2023Container') ) {
 else if ( document.getElementById('jan2024Container') ) {
     makeMonth( document.getElementById('jan2024Container'), 115, 'Jan' )
 }
+else if ( document.getElementById('feb2024Container') ) {
+    makeMonth( document.getElementById('feb2024Container'), 115, 'Feb', 2 )
+}
+
