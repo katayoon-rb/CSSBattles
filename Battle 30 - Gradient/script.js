05f28c0c72fd1930c3c0e6e2b7f4fb8ed82ae723
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-02-17 17:30:46 +0330

    Update script.js

--- Battle 30 - Gradient/script.js
+++ Battle 30 - Gradient/script.js
@@ -8,10 +8,11 @@ let list = [
 
 
 for (let i = 0; i < list.length; i++) {
+    let name = list[i].split(" ")[1] + ' ' +  list[i].split(" ")[2]
     container.innerHTML += `
         <div class="project">
             <div class="line">
-                <h2>${list[i].split(" ")[1]}</h2>
+                <h2>${name}</h2>
                 <a href="${list[i]}/index.html">
                     <button type="button">Go to Site</button>
                 </a>
