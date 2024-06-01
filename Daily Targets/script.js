772c7b10469b91382f8f6aa7f5c6b2ab63f4a56d
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-06-01 11:11:21 +0330

    Update script.js

--- Daily Targets/script.js
+++ Daily Targets/script.js
@@ -46,6 +46,9 @@ else if ( document.getElementById('apr2024Container') ) {
     makeMonth( document.getElementById('apr2024Container'), 206, 'Apr' )
 }
 else if ( document.getElementById('may2024Container') ) {
+    makeMonth( document.getElementById('apr2024Container'), 236, 'May' )
+}
+else if ( document.getElementById('june2024Container') ) {
     let today = parseInt(Date().split(' ')[2]) + 1
-    makeMonth( document.getElementById('apr2024Container'), 236, 'May', today )
+    makeMonth( document.getElementById('june2024Container'), 236, 'June', today )
 }
