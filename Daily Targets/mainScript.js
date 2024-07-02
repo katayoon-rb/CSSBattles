5dae9c15ed42228d7ebe1f1843b7d94c664ce0d5
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-07-02 12:02:48 +0330

    Update mainScript.js

--- Daily Targets/mainScript.js
+++ Daily Targets/mainScript.js
@@ -3,12 +3,12 @@ const months = ['september 2023', 'october 2023', 'november 2023', 'december 202
                 'january 2024', 'february 2024', 'march 2024', 'april 2024',
                 'may 2024', 'june 2024', 'july 2024']
 
-for (let month in months) {
+for (let i in months) {
     container.innerHTML += `
         <div class="project">
             <div class="line">
-                <h2>${month.split(" ")[0]}</h2>
-                <a href="${month}.html">
+                <h2>${months[i].split(" ")[0]}</h2>
+                <a href="${months[i]}.html">
                     <button type="button">View Projects</button>
                 </a>
             </div>
