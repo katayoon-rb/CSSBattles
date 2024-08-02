e1e72e5a454da204f1ad4dc3a8c64da16e2e0f31
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-08-02 10:31:23 +0330

    Update mainScript.js

--- Daily Targets/mainScript.js
+++ Daily Targets/mainScript.js
@@ -1,7 +1,7 @@
 const container = document.getElementById('container')
 const months = ['September 2023', 'October 2023', 'November 2023', 'December 2023',
                 'January 2024', 'February 2024', 'March 2024', 'April 2024',
-                'May 2024', 'June 2024', 'July 2024']
+                'May 2024', 'June 2024', 'July 2024', 'August 2024']
 
 for (let i in months) {
     container.innerHTML += `
