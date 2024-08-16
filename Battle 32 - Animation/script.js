a36f3d992edfcd284b45f546109aabd6df5e1e27
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-08-16 12:21:01 +0330

    Add files via upload

---
+++ Battle 32 - Animation/script.js
@@ -0,0 +1,22 @@
+const container = document.getElementById("container");
+let list = [
+  "01 Wolverine",
+  "02 Deadpool"
+];
+
+for (let i = 0; i < list.length; i++) {
+  let name =
+    list[i].split(" ")[1] +
+    (list[i].split(" ")[2] ? ` ${list[i].split(" ")[2]}` : "");
+  container.innerHTML += `
+        <div class="project">
+            <div class="line">
+                <h2>${name}</h2>
+                <a href="${list[i]}/index.html">
+                    <button type="button">Go to Site</button>
+                </a>
+            </div>
+            <img src="${list[i]}/screen.jpg" />
+        </div>
+    `;
+}
